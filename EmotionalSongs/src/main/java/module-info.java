6227cc96e297967758ecc79da64b57c52ac7be4d
FrancescoVulcano <fvulcano02@gmail.com>
2022-08-01 21:35:44 +0200

    Revert "First Commit"
    This reverts commit f59ba258e13cbc0fa604346f247cf9989c3cbc5b.

--- EmotionalSongs/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.emotionalsongs {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.emotionalsongs to javafx.fxml;
-    exports com.example.emotionalsongs;
-}
